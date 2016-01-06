b225ccbe61e2000866a608515511a07a366432f7
ayatake ezaki <ayatake@gakkoproject.com>
2016-01-07 01:46:39 +0900

    Test

---
+++ statics/js/earthcompany.js
@@ -0,0 +1,87 @@
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".scroll-top").fadeIn('1000', "easeInOutExpo");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".scroll-top").fadeOut('1000', "easeInOutExpo");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.overlay-menu ul li a,.scroll-top a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// WOW.js initialise
+// WOW.js uses animate.css to animate/reveal elements.
+// Browse the list of animation effects available here-> https://daneden.github.io/animate.css/
+$(function() {
+    wow = new WOW(
+    {
+      boxClass:     'wow',      // default
+      animateClass: 'animated', // default
+      offset:       0,          // default
+      mobile:       true,       // default
+      live:         true        // default
+    }
+  )
+  wow.init();
+});
+
+
+// jQuery Parallax. More info here-> https://github.com/IanLunn/jQuery-Parallax
+$(function(){
+    // apply parallax effect only when body has the ".parallax-page" class
+    if ($('body').hasClass('parallax-page')) {
+        //.parallax(xPosition, speedFactor, outerHeight) options:
+        //xPosition - Horizontal position of the element
+        //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+        //outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+        
+        $('#parallax-slide').parallax("50%", 0.1);
+        $('#products').parallax("50%", 0.1);
+        $('#portfolio').parallax("50%", 0.1);
+        $('#page-aboutus').parallax("50%", 0.1);
+    }
+});
+
+
+// Closes the Responsive Menu on Menu Item Click
+$('.overlay-menu ul li a').click(function() {
+    $('.button_container:visible').click();
+});
+
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// Navigation show/hide
+$('.toggle').click(function() {
+    if ($('#overlay.open')) {
+        $(this).toggleClass('active');
+        $('#overlay').toggleClass('open');
+    }
+});
+
