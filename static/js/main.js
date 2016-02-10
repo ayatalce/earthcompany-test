64669066c40f4d2717c068c2617c06ddfa7397dc
ayatake ezaki <ayatake@gakkoproject.com>
2016-02-10 10:29:16 +0900

    Add Profile Section

--- static/js/main.js
+++ static/js/main.js
@@ -34,3 +34,18 @@ function retina() {
 };
  
 $(document).ready(retina);
+
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+$('#myTabs a[href="#profile"]').tab('show') // Select tab by name
+$('#myTabs a:first').tab('show') // Select first tab
+$('#myTabs a:last').tab('show') // Select last tab
+$('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  e.target // newly activated tab
+  e.relatedTarget // previous active tab
+})
